015210dcc1a39efc8cc3e79ffd8b073fc25d0149
N-D-Duy <Ct060311@actvn.edu.vn>
2025-03-18 11:00:35 +0700

    feat: close socket after exit application

--- src/main/java/org/duynguyen/atttclient/HelloApplication.java
+++ src/main/java/org/duynguyen/atttclient/HelloApplication.java
@@ -10,24 +10,42 @@ import org.duynguyen.atttclient.network.Session;
 import org.duynguyen.atttclient.presentation.widgets.GlobalUI;
 
 public class HelloApplication extends Application {
-  @Getter public static Stage primaryStage;
+    @Getter
+    public static Stage primaryStage;
+    private static Session session;
 
-  @Override
-  public void start(Stage stage) throws IOException {
-    primaryStage = stage;
-    FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("startup.fxml"));
-    Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-    stage.setTitle("Hello!");
-    stage.setScene(scene);
-    GlobalUI.init(stage);
-    stage.show();
-  }
+    @Override
+    public void start(Stage stage) throws IOException {
+        primaryStage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("startup.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        GlobalUI.init(stage);
+        stage.setOnCloseRequest(event -> {
+            System.out.println("Closing application...");
+            closeSession();
+        });
+        stage.show();
+    }
 
-  public static void main(String[] args) {
-            Session session = new Session("127.0.0.1", 1690);
-//    Session session = new Session("20.243.124.24", 1690);
-    session.connect();
-    launch();
-  }
+    public static void main(String[] args) {
+        session = new Session("20.243.124.24", 1690);
+        session.connect();
+        launch();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        System.out.println("Application is stopping...");
+        closeSession();
+        super.stop();
+    }
+
+    private void closeSession() {
+        if (session != null) {
+            session.close();
+            System.out.println("Session closed.");
+        }
+    }
 }
-
